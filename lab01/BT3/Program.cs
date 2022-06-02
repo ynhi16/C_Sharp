76ff564cb7661c36cd27c6259872015fc44df682
y nhi <nguyenthiynhi010601@gmail.com>
2022-06-02 19:36:25 +0700

    lab01

---
+++ lab01/BT3/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace BT3
+{
+    class People
+    {
+        public int SID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+    }
+    class Student : People
+    {
+        public string Lop { get; set; }
+        public string Khoa { get; set; }
+        public double DiemTB { get; set; }
+
+        public void HienThi()
+        {
+            Console.WriteLine("Ma Sinh Vien: " + SID);
+            Console.WriteLine("Ho Ten: " + Name);
+            Console.WriteLine("Tuoi: " + Age);
+            Console.WriteLine("Gioi Tinh: " + Gender);
+            Console.WriteLine("Lop: " + Lop);
+            Console.WriteLine("Khoa: " + Khoa);
+            Console.WriteLine("Diem trung binh: " + DiemTB);
+        }
+        public static void listStudent(int n)
+        {
+            Student std = new Student();
+            List<Student> students = new List<Student>();
+            Console.WriteLine("========== Nhập thông tin sinh viên ==========");
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Sinh vien thu: " + i);
+                Console.Write(" Ma Sinh Vien: ");
+                std.SID = int.Parse(Console.ReadLine());
+                Console.Write(" Ho Ten: ");
+                std.Name = Console.ReadLine();
+                Console.Write(" Tuoi: ");
+                std.Age = Convert.ToInt32(Console.ReadLine());
+                Console.Write(" Gioi Tinh: ");
+                std.Gender = Console.ReadLine();
+                Console.Write(" Lop: ");
+                std.Lop = Console.ReadLine();
+                Console.Write(" Khoa: ");
+                std.Khoa = Console.ReadLine();
+                Console.Write(" Diem trung binh: ");
+                std.DiemTB = Convert.ToDouble(Console.ReadLine());
+                students.Add(std);
+                std = new Student();
+            }
+            Console.WriteLine("========== Hiển thị thông tin sinh viên ==========");
+            for (int i = 0; i < n; i++)
+            {
+                students.ElementAt(i).HienThi();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        public static void arrListStudent(int n)
+        {
+            Student std = new Student();
+            ArrayList students = new ArrayList();
+            Console.WriteLine("========== Nhập thông tin sinh viên ==========");
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Sinh vien thu: " + i);
+                Console.Write(" Ma Sinh Vien: ");
+                std.SID = int.Parse(Console.ReadLine());
+                Console.Write(" Ho Ten: ");
+                std.Name = Console.ReadLine();
+                Console.Write(" Tuoi: ");
+                std.Age = Convert.ToInt32(Console.ReadLine());
+                Console.Write(" Gioi Tinh: ");
+                std.Gender = Console.ReadLine();
+                Console.Write(" Lop: ");
+                std.Lop = Console.ReadLine();
+                Console.Write(" Khoa: ");
+                std.Khoa = Console.ReadLine();
+                Console.Write(" Diem trung binh: ");
+                std.DiemTB = Convert.ToDouble(Console.ReadLine());
+                students.Add(std);
+                std = new Student();
+            }
+            Console.WriteLine("========== Hiển thị thông tin sinh viên ==========");
+            foreach (Student student in students)
+            {
+                student.HienThi();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.Write("Nhap so sinh vien: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write("1.List 2.Arraylist ");
+                int kieu = int.Parse(Console.ReadLine());
+                switch (kieu)
+                {
+                    case 1:
+                        listStudent(n);
+                        break;
+                    case 2:
+                        arrListStudent(n);
+                        break;
+                    default:
+                        Console.WriteLine("Vui lòng nhập lại");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ lab01/lab01/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab01
+{
+    class Student
+    {
+        private int SID;
+        private string TenSV;
+        private string Khoa;
+        private float DiemTB;
+        public Student() //Constructor mac dinh
+        {
+            SID = 1;
+            TenSV = "Nguyen Van Nam";
+            Khoa = "CNTT";
+            DiemTB = 7;
+        }
+        public Student(Student stu) //Constructor sao chep
+        {
+            SID = stu.SID;
+            TenSV = stu.TenSV;
+            Khoa = stu.Khoa;
+            DiemTB = stu.DiemTB;
+        }
+        //Constructor tham so
+        public Student(int id, string ten, string kh, float dtb)
+        {
+            SID = id;
+            TenSV = ten;
+            Khoa = kh;
+            DiemTB = dtb;
+        }
+        //Cac Property cho tung thuoc tinh cua lop
+        public int StudentID //Property dai dien cho thuoc tinh SID
+        {
+            get { return SID; } //Lay du lieu
+            set { SID = value; } //Gan du lieu
+        }
+        public String Name
+        {
+            get { return TenSV; }
+            set { TenSV = value; }
+        }
+        public String Faculty
+        {
+            get { return Khoa; }
+            set { Khoa = value; }
+        }
+        public float Mark
+        {
+            get { return DiemTB; }
+            set { DiemTB = value; }
+        }
+        //Xuat
+        public void Nhap1SV()
+        {
+            Console.Write("Nhap Ma SV:");
+            SID = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap Ho Ten:");
+            TenSV = Console.ReadLine();
+            Console.Write("Nhap Khoa:");
+            Khoa = Console.ReadLine();
+            Console.Write("Nhap Diem TB:");
+            DiemTB = Convert.ToSingle(Console.ReadLine());
+        }
+        public void NhapDS(Student[] DSSV, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap Thong Tin SV THU " + (i + 1));
+                DSSV[i] = new Student();
+                DSSV[i].Nhap1SV();
+            }
+        }
+        //Phuong thuc hien thi du lieu
+        public void Xuat1SV()
+        {
+            Console.WriteLine("MSSV:{0}", this.SID);
+            Console.WriteLine("Ten SV:{0}", this.TenSV);
+            Console.WriteLine("Khoa:{0}", this.Khoa);
+            Console.WriteLine("Diem TB:{0}", this.DiemTB);
+        }
+        public void XuatDS(Student[] DSSV, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                DSSV[i].Xuat1SV();
+            }
+        }
+    }
+    class Tester
+    {
+        public static void Main()
+        {
+            Student[] DSSV;
+            int n;
+            Student sv;
+            sv = new Student();
+            Console.Write("Nhap so luong SV: ");
+            n = int.Parse(Console.ReadLine());
+            DSSV = new Student[n]; //tao mang n phan tu
+            // Nhap DS Sinh vien
+            Console.WriteLine("\n ====NHAP DS SINH VIEN====");
+            sv.NhapDS(DSSV,n);
+
+            //Xuat DS Sinh vien
+            Console.WriteLine("\n ====XUAT DS SINH VIEN====");
+            sv.XuatDS(DSSV, n);
+            Console.ReadLine();
+        }
+    }
+}
+
